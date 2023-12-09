0ad5a66aaccd3d481b7bb1f25f4be095dc71892e
Ezequiel Mensur <eze-mensur@hotmail.es>
2023-12-09 17:04:20 -0300

    Feat(staking): initial smart contract is created

---
+++ tests/staking_contract_scenario_rs_test.rs
@@ -0,0 +1,15 @@
+use multiversx_sc_scenario::*;
+
+fn world() -> ScenarioWorld {
+    let mut blockchain = ScenarioWorld::new();
+    // blockchain.set_current_dir_from_workspace("relative path to your workspace, if applicable");
+
+    blockchain.register_contract("file:output/staking-contract.wasm", staking_contract::ContractBuilder);
+    blockchain
+}
+
+#[test]
+fn empty_rs() {
+    world().run("scenarios/staking_contract.scen.json");
+}
+
